fab6ae96c2d0632920badcc9c3cbcc94cc755dac
Elijah Granet <ezgranet@gmail.com>
2022-11-06 12:35:56 +0000

    more js

--- better-austlii/styles.js
+++ better-austlii/styles.js
@@ -1,13 +1,14 @@
 function numberOrderedList(ol) {
-	ol.classList.add('paragraphs');
-	let liCount = ;
-	ol.childNodes.forEach(function (node) {
-		if (node.nodeName !== 'LI') {
-			return;
-		}
-		liCount++;
-		node.innerHTML = '<span class="paragraph_number"><b>[' + ( liCount ) + ']&nbsp;</b></span>' + node.innerHTML;
-	});
+ol.classList.add('paragraphs');
+let liCount = 0;
+ol.childNodes.forEach(function (node) {
+if (node.nodeName !== 'LI') {
+return;
 }
-let olNodes = Array.from(document.getElementsByTagName('ol'));
-olNodes.forEach(ol => numberOrderedList(ol));
+liCount++;
+if (node.hasAttribute('value')) {
+            liCount = parseInt(node.getAttribute('value'), 10);
+        }
+        node.innerHTML = '<span class="paragraph_number"><b>[' + ( liCount ) + ']&nbsp;</b></span>' + node.innerHTML;
+});
+}
